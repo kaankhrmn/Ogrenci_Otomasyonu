fd5d9a3f54f2590f3ea8df2511918ebe131b5ac2
kaank <kaank@KAAN>
2024-02-19 19:00:27 +0300

    Add project files.

---
+++ Ogrenci_Kayit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Öğrenci_Otomasyonu
+{
+    public class Ogrenci_Kayit
+    {
+
+        public int OgrenciNumarasi { get; set; }
+        public string OgrenciAdiSoyadi { get; set; }
+        public short OgrenciVizeNotu { get; set; }
+        public short OgrenciFinalNotu { get; set; }
+        public string OgrenciDersDurum { get; set; }
+
+
+        public Ogrenci_Kayit(int OgrenciNumarasi)
+        {
+            this.OgrenciNumarasi = OgrenciNumarasi;
+        }
+
+
+        public void OgrenciEkle(int OgrenciNumarasi, string OgrenciAdiSoyadi, short OgrenciVizeNotu, short OgrenciFinalNotu, string OgrenciDersDurum)
+        {
+            Console.WriteLine(OgrenciNumarasi + " " + OgrenciAdiSoyadi + " " + OgrenciVizeNotu + " " + OgrenciFinalNotu + " " + OgrenciDersDurum + " " + "eklendi... ");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Öğrenci_Otomasyonu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrenci Kayıt Otomasyonuna Hoş Geldiniz");
+            Console.WriteLine("1-Yeni Öğrenci Ekle");
+            Console.WriteLine("2-Öğrenci Sil");
+            Console.WriteLine("3-Öğrenci Sayısını Bul");
+            Console.WriteLine("Öğrenci Ara");
+            Console.WriteLine("Tüm Öğrencileri Listele");
+            Console.WriteLine("6-Çıkış Yap");
+
+
+
+            int tuslanan =Convert.ToInt32(Console.ReadLine());
+
+            switch (tuslanan)
+            {
+                case 1:
+                        
+                    break;
+                    
+
+                    
+            }
+
+
+
+
+        }
+    }
+}
